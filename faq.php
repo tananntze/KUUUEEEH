b9e3e453418b160875f26cdc8b4d11493274263f
kicku6 <blazeking990@gmail.com>
2019-10-05 19:24:11 +0800

    Andrew give it a try

--- faq.php
+++ faq.php
@@ -4,11 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="http://localhost/Contactus/main.css">
+        <link rel="stylesheet" href="http://localhost/1004-ProjectFiles/KUUUEEEH/main.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"   >
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script defer src="http://localhost/Contactus/main.js"></script>
+        <script defer src="http://localhost/1004-ProjectFiles/KUUUEEEH/main.js"></script>
     </head>
 
     <body class="overlay">
