b168ad7ab4cb8521c84f71b35a1642ab3795304d
kicku6 <blazeking990@gmail.com>
2019-10-05 18:37:25 +0800

    05102019

---
+++ 1004-Project/faq.php
@@ -0,0 +1,83 @@
+<html class="header">
+    <head>
+        <title>FAQ</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://localhost/Contactus/main.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"   >
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script defer src="http://localhost/Contactus/main.js"></script>
+    </head>
+
+    <body class="overlay">
+        <header >
+            <nav class="nav navbar navbar-expand-md bg-pink navbar-dark"  role ="navigation">
+                <a class="navbar-brand" href="#">Kuey Logo</a>
+                <button class ="navbar-toggler" type = 'button' data-toggle="collapse" data-target ="#navbar">
+                    <span class ="navbar-toggler-icon"></span>
+                </button>
+                <div class ="collapse navbar-collapse" id="navbar">
+                    <ul class="nav navbar-nav">
+                        <li class ="nav-item"><a href ="#">Home</a></li>
+                        <li class ="nav-item"><a href="#">About Us</a></li>
+                        <li class ="nav-item"><a href="#">Contact Us</a></li>
+                        <li class ="nav-item"><a href="#">FAQ's</a></li>
+
+                    </ul>
+                    <ul class ="nav navbar-nav ml-auto">
+                        <li class="nav navbar-nav">
+                            <a class ="nav-link" href="#"><span class="fas fa-directions"></span> Login</a>
+                        </li>
+                    </ul>
+                </div>  
+            </nav>
+        </header>
+        <div class="jumbotron text-center jumbotronbg" style = "margin-bottom:0"> 
+            <h1>Kuey Lai Liao</h1>
+            <h3>The legendary area of kueys</h3>
+        </div>
+        <section>
+            <div class="container-fluid" style= 'margin-top:20px'>
+                <div class =" row sectionheader standardfont text-center">
+                    <div class="col-md-12">
+                        <h1 class = fontheader>FAQ</h1>
+                    </div>
+                </div>
+                <div class ="row justify-content-left standardfont">
+                    <div class ="col-md-4">
+                        <h3 class ="subheader ml-4">Ordering Information</h3>
+                        <p >Telephone number: +65 9212 9999</p>
+                    </div>
+                    <div class ="col-md-4">
+                        <h3 class ="subheader"> Product Information </h3>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div class ="col-md-4">
+                        <h3 class ="subheader"> Events Information </h3>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+<!--                <div class ="row justify-content-center ml-4 standardfont pt-5">
+
+
+                    <div class =' col-md-4'>
+
+                    </div>
+                    <div class =' col-md-4'>
+                        <h3 class ="fontheader">Get in Touch with us here!</h3>
+                    </div>
+                    <div class =' col-md-4'>
+
+                    </div>
+
+
+                </div>-->
+        </section>
+    </body>
+</html>
