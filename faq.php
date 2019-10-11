a66e0cbe562a44f350702df1a611cf466a3ddad1
Jerone Poh <pohjerone97@gmail.com>
2019-10-11 14:18:58 +0800

    Added credits from the banner

--- faq.php
+++ faq.php
@@ -81,6 +81,7 @@
         </section>
 
         <div class="container">
+            <!--The animated kueh images for the banner are taken and credited by ladyironchef: Beginner’s Guide to Kuehs – 9 Traditional Kuehs You Must Try https://www.ladyironchef.com/2015/08/guide-traditional-kueh/-->
             <img src="img/Banner - White.png" alt="bannerimg" class="responsive" id="bannerresize">
         </div>
 
