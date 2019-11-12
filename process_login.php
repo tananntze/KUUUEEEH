2ccd140e487e3fd8f9af6a29ddd819b1b461a972
Andrew <tananntze@gmail.com>
2019-11-12 23:31:03 +0800

    Updated process login
    Hashed password into db

--- process_login.php
+++ process_login.php
@@ -1,4 +1,15 @@
 <?php
+
+    /*
+        User Account 1
+        username: testing@gmail.com
+        password: 123456
+
+        User Account 2
+        username: test@gmail.com
+        password: admin
+    */
+
     require "dbConfig.php";
     include "header.php";
 
@@ -33,7 +44,7 @@
     }
 
     function validate_user(){
-        $email = $password = $errorMsg = "";
+        $email = $password = $errorMsg = $hashedpassword = "";
         $success = true;
 
         if (empty($_POST["email"])){
@@ -57,10 +68,11 @@
         else
         {
             $password = sanitize_input($_POST["password"]);
+            $hashedpassword = md5($password);
         }
 
         if ($success){
-            login($email,$password);
+            login($email,$hashedpassword);
         }else{
             echo "<h4>The following input errors were detected:</h4>";
             echo "<p>" . $errorMsg . "</p>";
